31cc46579ad432d89022a638c470a26012df9ae1
CHENCHAO0526 <949523841@qq.com>
2022-05-08 20:15:58 +0800

    init

---
+++ log.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- encoding=utf8 -*-
+import logging
+import logging.handlers
+from time import strftime
+import os
+import os.path as osp
+
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+LOG_FILENAME = strftime(osp.join(LOG_DIR, "cc-shoping_%Y_%m_%d_%H.log"))
+
+
+def setup_custom_logger(name):
+    formatter = logging.Formatter('%(asctime)s %(filename)s：%(lineno)d %(levelname)s : %(message)s')
+
+    handler = logging.StreamHandler()
+    file_handler = logging.handlers.RotatingFileHandler(
+        LOG_FILENAME, maxBytes=10485760, backupCount=5, encoding="utf-8")
+
+    file_handler.setFormatter(formatter)
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+    logger.addHandler(file_handler)
+    return logger
+
+logger = setup_custom_logger("cc-shoping")
+
